52401c2d7a3728f72260d8be3379987c2b192e80
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-09-05 20:16:49 +0100

    trying to do data searching through rust

--- src/logic/request.ts
+++ src/logic/request.ts
@@ -1,36 +1,50 @@
 type WeatherData = {
-  latitude: number;
-  longitude: number;
-  generationtime_ms: number;
-  utc_offset_seconds: number;
-  timezone: string;
-  timezone_abbreviation: string;
-  elevation: number;
-  hourly_units: {
-    time: string;
-    temperature_2m: string;
-    precipitation_probability: string;
-    weather_code: string;
-    wind_speed_10m: string;
-  };
-  hourly: {
-    time: string[];
-    temperature_2m: number;
-    precipitation_probability: number;
-    weather_code: number;
-    wind_speed_10m: number;
-  };
+    latitude: number;
+    longitude: number;
+    generationtime_ms: number;
+    utc_offset_seconds: number;
+    timezone: string;
+    timezone_abbreviation: string;
+    elevation: number;
+    hourly_units: {
+        time: string[];
+        temperature_2m: string[];
+        precipitation_probability: string[];
+        weather_code: string[];
+        wind_speed_10m: string[];
+    };
+    hourly: {
+        time: string[];
+        temperature_2m: number[];
+        precipitation_probability: number[];
+        weather_code: number[];
+        wind_speed_10m: number[];
+    };
 };
 
-let weather_data: WeatherData;
+let weatherData: WeatherData | null = null;
 
 export async function requestWeather() {
-  const request = await fetch(
-    'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,weather_code,wind_speed_10m&forecast_days=1'
-  );
-  const response = await request.text();
+    const request = await fetch(
+        'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,weather_code,wind_speed_10m&forecast_days=1'
+    );
+    const response = await request.text();
 
-  weather_data = JSON.parse(response);
+    weatherData = JSON.parse(response);
 }
 
-function getData(what: string, when: string) {}
+type WhatKey = keyof WeatherData['hourly'];
+
+// maybe use a rust loop
+
+function getData(what: WhatKey, when: string): string | number | null {
+    if (weatherData !== null) {
+        const timeStrings: string[] = weatherData.hourly.time;
+        for (let i = 0; i > timeStrings.length; i++) {
+            if (timeStrings[i] === when) {
+                return weatherData.hourly[what][i];
+            }
+        }
+    }
+    return null;
+}
