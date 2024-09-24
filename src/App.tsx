8ff7bea11c558e90ed670b253ca87f718fede4df
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-09-24 11:25:46 +0100

    added tomorrow to next but untested

--- src/App.tsx
+++ src/App.tsx
@@ -6,46 +6,28 @@ import { useWeather } from './logic/hooks';
 import Next from './components/Next';
 
 export default function App(): React.JSX.Element {
-    const weather = useWeather();
+  const weather = useWeather();
 
-    return (
-        <>
-            <div id='top'>
-                <div id='left'>
-                    <div
-                        id='row'
-                        style={{ gap: 20 }}
-                    >
-                        <CurrentStat
-                            value={weather.now.temperature}
-                            type='temp'
-                        />
-                        <CurrentStat
-                            value={weather.now.precipitationProb}
-                            type='prec'
-                        />
-                    </div>
-                    <div
-                        id='row'
-                        style={{ gap: 20 }}
-                    >
-                        <CurrentStat
-                            value={weather.now.weatherCode}
-                            type='code'
-                        />
-                        <CurrentStat
-                            value={weather.now.windSpeed}
-                            type='wind'
-                        />
-                    </div>
-                </div>
-                <div id='right'>
-                    <Time />
-                </div>
-            </div>
-            <div id='bottom'>
-                <Next data={weather.day} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div id='top'>
+        <div id='left'>
+          <div id='row' style={{ gap: 20 }}>
+            <CurrentStat value={weather.now.temperature} type='temp' />
+            <CurrentStat value={weather.now.precipitationProb} type='prec' />
+          </div>
+          <div id='row' style={{ gap: 20 }}>
+            <CurrentStat value={weather.now.weatherCode} type='code' />
+            <CurrentStat value={weather.now.windSpeed} type='wind' />
+          </div>
+        </div>
+        <div id='right'>
+          <Time />
+        </div>
+      </div>
+      <div id='bottom'>
+        <Next weatherData={weather} />
+      </div>
+    </>
+  );
 }
--- src/components/Next.tsx
+++ src/components/Next.tsx
@@ -1,49 +1,69 @@
 import React, { useEffect, useState } from 'react';
-import { TimeWeather } from '../logic/types';
+import { TimeWeather, WeatherData } from '../logic/types';
 import { useTime } from '../logic/hooks';
 import Icon from './Icon';
 
-type Types = { data: TimeWeather[] };
+type Types = {
+  weatherData: {
+    all: WeatherData;
+    now: TimeWeather;
+    day: TimeWeather[];
+    tomorrow: TimeWeather[];
+  };
+};
 
-export default function Next({ data }: Types): React.JSX.Element {
-    const [display, setDisplay] = useState<TimeWeather[]>();
-    const time = useTime();
-    useEffect(() => {
-        const getStartStop = () => {
-            let start = -1;
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].time === time.split(':')[0] + ':00') {
-                    start = i + 1;
-                }
-            }
-            let stop = start + 5;
-            return { start, stop };
-        };
+export default function Next({ weatherData }: Types): React.JSX.Element {
+  const [display, setDisplay] = useState<TimeWeather[]>([]);
+  const time = useTime();
+  useEffect(() => {
+    const day = weatherData.day;
 
-        const indices = getStartStop();
-        setDisplay(data.slice(indices.start, indices.stop));
+    const getStartStop = () => {
+      let start = -1;
+      for (let i = 0; i < day.length; i++) {
+        if (day[i].time === time.split(':')[0] + ':00') {
+          start = i + 1;
+        }
+      }
+      let stop = start + 5;
+      return { start, stop };
+    };
 
-    }, [data]);
+    const addTomorrow = () => {
+      const difference = 5 - display.length;
+      const tomorrowArray: TimeWeather[] = [];
 
-    return (
-        <div style={{ flexDirection: 'row', display: 'flex' }}>
-            {display?.map((item, index) => (
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 0,
-                    }}>
-                    <Icon
-                        key={index}
-                        temperature={item.temperature}
-                        weatherCode={item.weatherCode}
-                    />
-                    <p style={{ margin: 0, textAlign: 'center' }}>
-                        {item.time}
-                    </p>
-                </div>
-            ))}
+      if (difference > 0) {
+        for (let i = 0; i < difference; i++) {
+          tomorrowArray.push(weatherData.tomorrow[i]);
+        }
+      }
+
+      setDisplay(display.concat(tomorrowArray));
+    };
+    const indices = getStartStop();
+    setDisplay(day.slice(indices.start, indices.stop)); //on launch starts at 01:00
+    addTomorrow();
+  }, [weatherData]);
+
+  return (
+    <div style={{ flexDirection: 'row', display: 'flex' }}>
+      {display?.map((item, index) => (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 0,
+          }}
+        >
+          <Icon
+            key={index}
+            temperature={item.temperature}
+            weatherCode={item.weatherCode}
+          />
+          <p style={{ margin: 0, textAlign: 'center' }}>{item.time}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
